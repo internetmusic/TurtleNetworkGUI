339fb194c173715477125cb2bfcba16e129a86ef
Tony Nguyen <tnguyen@etisalat.ae>
2020-03-15 02:18:55 +0400

    Update regex for ETHO & Change Landing

--- src/modules/utils/services/outerBlockchains.js
+++ src/modules/utils/services/outerBlockchains.js
@@ -8,7 +8,7 @@
             [WavesApp.defaultAssets.WAVES]: /^3P[a-km-zA-HJ-NP-Z1-9]{33}/,
             [WavesApp.defaultAssets.TUSD]: /^0x[0-9a-f]{40}$/i,
             [WavesApp.defaultAssets.DOGE]: /^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}/,
-            [WavesApp.defaultAssets.ETHO]: /^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}/,
+            [WavesApp.defaultAssets.ETHO]: /^0x[0-9a-f]{40}$/i,
             [WavesApp.defaultAssets.SENT]: /^0x[0-9a-f]{40}$/i
         };
 
