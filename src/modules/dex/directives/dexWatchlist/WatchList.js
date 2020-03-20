01ee4d78868f878447173c1647bfa1351d1acbb2
Tony Nguyen <tnguyen@etisalat.ae>
2020-03-20 20:20:20 +0400

    Fix for #87

--- src/modules/dex/directives/dexWatchlist/WatchList.js
+++ src/modules/dex/directives/dexWatchlist/WatchList.js
@@ -15,6 +15,16 @@
     });
     DROP_DOWN_LIST.push({ name: 'ALL', id: 'all' });
 
+    DROP_DOWN_LIST.sort((a, b) => {
+        if (a.name < b.name) {
+            return -1;
+        }
+        if (a.name > b.name) {
+            return 1;
+        }
+        return 0;
+    });
+
     /**
      * @param Base
      * @param {$rootScope.Scope} $scope
